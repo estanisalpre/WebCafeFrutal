93a140261ee43d76827eb91ddc6df7418a31b0e5
Estanislao Salinas <estanisprevite@hotmail.com>
2024-09-01 15:14:24 -0500

    changes in branches

--- public/app.js
+++ public/app.js
@@ -15,7 +15,7 @@ io.on('connection', (socket) => {
     });
 })
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'index.html')));
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`)
