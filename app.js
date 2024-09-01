a40aa1440ca684db898eacecc110c0f9c0747122
Estanislao Salinas <estanisprevite@hotmail.com>
2024-09-01 15:42:44 -0500

    routes modifications

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ io.on('connection', (socket) => {
     });
 })
 
-app.use(express.static(path.join(__dirname, 'public', 'index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`)
